3c005d0a2cd6c78f65766ab1c8b41eb93b0f4084
Florian Gümbel <florian_guembel1@web.de>
2021-08-03 21:01:16 +0200

    Added base64 encoding to fileserver

--- routes/fileserver.js
+++ routes/fileserver.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 // yes we have a LFI here
 router.get('/', function(req, res, next) {
-    res.sendFile(req.query.file);
+    const path = Buffer.from(req.query.file, 'base64').toString('utf8');
+    res.sendFile(path);
 });
 
 module.exports = router;
